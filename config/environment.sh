ed5b0d454b148cb50eedb4452e5369e84f3977c7
Karikko Mika-Matti K <karikko@da5-cs-b221-03.helsinki.fi>
2016-04-01 13:55:31 +0300

    Sovellus alustettu ja versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="karikko"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="Muistilista"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
